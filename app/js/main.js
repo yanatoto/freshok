a4af553ec31ace1a289d5f516a95c75e83ca7bf9
yanatoto <yanas@bluewin.ch>
2022-01-29 16:54:57 +0100

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.slider__inner').slick({
+    dots: false,
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--left"></button>',
+    nextArrow: '<button type="button" class= "slider__arrow slider__arrow--right"></button>'
+  });
+
+var mixer = mixitup('.top__content');
+  
+});
+
