c31bfb5517182d91e6554bb99165a05ccf71d94d
yanatoto <yanas@bluewin.ch>
2022-02-10 18:17:35 +0100

    added promo

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,17 @@ $(function () {
     nextArrow: '<button type="button" class= "slider__arrow slider__arrow--right"></button>'
   });
 
-var mixer = mixitup('.top__content');
   
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
 
